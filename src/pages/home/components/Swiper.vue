95ab6b195e1ee79e9c344be8598c54a3aa1d0420
wmy <circle_w@126.com>
2018-11-21 10:51:34 +0800

    changeSwiper

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,53 @@
+<template>
+<div class="wrapper">
+    <swiper :options="swiperOption">
+        <swiper-slide v-for="item in swiperList" :key="item.id">
+            <img class="swiper-img" :src="item.imgUrl">
+        </swiper-slide>
+       
+        <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+</div>
+</template>
+
+<script>
+export default {
+  name: "HomeSwiper",
+  data() {
+    return {
+      swiperOption: {
+        pagination: ".swiper-pagination",
+        loop:true,
+      },
+      swiperList: [
+        {
+          id: "0001",
+          imgUrl: "http://img1.qunarzz.com/piao/fusion/1810/6e/4a5551bbb189d902.jpg_750x200_25257467.jpg"
+        },
+        {
+          id: "0002",
+          imgUrl: "http://img1.qunarzz.com/piao/fusion/1806/1c/4847ea66072c7b02.jpg_750x200_c32457fb.jpg"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="stylus" scoped>
+.wrapper >>> .swiper-pagination-bullet-active {
+    background: #fff;
+}
+
+.wrapper {
+    overflow: hidden;
+    width: 100%;
+    height: 0;
+    padding-bottom: 26.67%;
+    background: #eee;
+
+    .swiper-img {
+        width: 100%;
+    }
+}
+</style>
