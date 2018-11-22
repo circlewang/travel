c1522ad6a816f9be107621cea141841c38fc2f3c
wmy <circle_w@126.com>
2018-11-22 11:37:54 +0800

    add citydata

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,15 +2,44 @@
     <div>
         <city-header></city-header>
         <city-search></city-search>
+        <city-list :cities="cities" :hotCities="hotCities"></city-list>
+        <city-alphabet :cities="cities" ></city-alphabet>    
     </div>
 </template>
 
 <script>
 import CityHeader from './components/Header'
 import CitySearch from './components/Search'
+import CityList from './components/List'
+import CityAlphabet from './components/Alphabet'
+import axios from 'axios'
 export default {
     name:'City',
-    components:{CityHeader,CitySearch}
+    components:{CityHeader,CitySearch,CityList,CityAlphabet},
+    data(){
+        return{
+            cities:{},
+            hotCities:[]
+        }
+    },
+    mounted(){
+        this.getCityInfo();
+        
+    },
+    methods:{
+        getCityInfo(){
+            axios.get("/api/city.json").then(this.handleGetCityInfoSucc)
+        },
+        handleGetCityInfoSucc(res){
+            console.log(res)
+            res=res.data
+            if(res.ret && res.data){
+                const data=res.data
+                this.cities=data.cities;
+                this.hotCities=data.hotCities
+            }
+        }
+    }
 }
 </script>
 
