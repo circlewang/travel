c5baddc0dadb7069d43d7d08a3e92502f5ecc3f5
wmy <circle_w@126.com>
2018-11-23 10:33:13 +0800

    finish gallary

--- src/pages/home/components/Recommend.vue
+++ src/pages/home/components/Recommend.vue
@@ -4,7 +4,7 @@
         热销推荐
     </div>
     <ul>
-        <li class="item border-bottom" v-for="item in list" :key="item.id">
+        <router-link tag="li" v-for="item in list" :to="'/detail/'+item.id" class="item border-bottom"  :key="item.id">
             <div class="item-img-wrapper">
                 <img class="item-img" :src="item.imgUrl">
             </div>
@@ -13,7 +13,7 @@
                 <p class="item-desc">{{item.desc}}</p>
                 <button class="item-button">查看详情</button>
             </div>
-        </li>
+        </router-link>
     </ul>
 </div>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/pages/home/Home'
 import City from '@/pages/city/City'
+import Detail from '@/pages/detail/Detail'
 
 Vue.use(Router)
 
@@ -13,8 +14,12 @@ export default new Router({
       component: Home
     },{
       path: '/city',
-      name: 'city',
+      name: 'City',
       component: City
+    },{
+      path: '/detail/:id',
+      name: 'Detail',
+      component: Detail
     }
   ]
 })
