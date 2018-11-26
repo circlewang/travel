d9939d5afb2d94d2891df46701dd84909227b45b
wmy <circle_w@126.com>
2018-11-26 09:37:32 +0800

    finish

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,7 +1,7 @@
 <template>
     <ul class="list" >
         <li class="item" v-for="item in letters" :key="item" :ref="item"
-        @touchstart="handleTouchStart" 
+        @touchstart.prevent="handleTouchStart" 
         @touchmove="handleTouchMove"
         @touchend="handleTouchEnd"
         @click="handleLetterClick">
